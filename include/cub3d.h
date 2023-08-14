b1382c76b6364dd9b079e5b9adbe50618cc2009a
Eva Van der neut <evan-der@f1r5s11.codam.nl>
2023-08-14 18:16:57 +0200

    kus

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,17 @@
 /*   By: opelser <opelser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/14 17:14:43 by opelser       #+#    #+#                 */
-/*   Updated: 2023/08/14 17:14:44 by opelser       ########   odam.nl         */
+/*   Updated: 2023/08/14 18:11:15 by evan-der      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct	s_map
+{
+	char	**map;
+	int		x;
+	int		y;
+}				t_map;
