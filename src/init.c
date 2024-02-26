ba68fe6d383ad1a501afaa021c5a5f48578f2893
olebol <ole.pelser@gmail.com>
2024-02-26 22:28:32 +0100

    Refactor init_mlx_data function to initialize screen and minimap

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: opelser <opelser@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 14:32:27 by opelser           #+#    #+#             */
-/*   Updated: 2024/02/15 15:38:21 by opelser          ###   ########.fr       */
+/*   Updated: 2024/02/26 21:51:47 by opelser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,24 +28,53 @@ int		init_player(t_data *data)
 	return (0);
 }
 
-int		init_mlx_data(t_data *data)
+static bool		init_screen(t_data *data)
 {
-	data->mlx = mlx_init(WIN_WIDTH, WIN_HEIGHT, "Cub3D", false);
-	if (!data->mlx)
-		return (1);
 	data->screen = mlx_new_image(data->mlx, WIN_WIDTH, WIN_HEIGHT);
 	if (!data->screen)
 	{
 		mlx_terminate(data->mlx);
-		return (1);
+		return (false);
 	}
 	if (mlx_image_to_window(data->mlx, data->screen, 0, 0) == -1)
 	{
+		mlx_delete_image(data->mlx, data->screen);
 		mlx_terminate(data->mlx);
-		return (2);
+		return (false);
 	}
+	return (true);
+}
 
-	data->map.tile_size = 32;
-	data->map.minimap_size = WIN_WIDTH / 8;
+static bool		init_minimap(t_data *data)
+{
+	const uint32_t		minimap_size = (data->screen->width / 8 + data->screen->height / 8) / 2;
+
+	data->minimap = mlx_new_image(data->mlx, minimap_size, minimap_size);
+	if (!data->minimap)
+	{
+		mlx_delete_image(data->mlx, data->screen);
+		mlx_terminate(data->mlx);
+		return (false);
+	}
+	if (mlx_image_to_window(data->mlx, data->minimap, 10, 10) == -1)
+	{
+		mlx_delete_image(data->mlx, data->screen);
+		mlx_delete_image(data->mlx, data->minimap);
+		mlx_terminate(data->mlx);
+		return (false);
+	}
+	return (true);
+}
+
+int		init_mlx_data(t_data *data)
+{
+	data->mlx = mlx_init(WIN_WIDTH, WIN_HEIGHT, "Cub3D", false);
+	if (!data->mlx)
+		ft_error("MLX failed to initialize");
+	if (!init_screen(data))
+		ft_error("Screen failed to initialize");
+	if (!init_minimap(data))
+		ft_error("Minimap failed to initialize");
+
 	return (0);
 }
