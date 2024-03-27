7ed6342c98c68847fd0ce63896acdd228b182b61
olebol <ole.pelser@gmail.com>
2024-03-27 18:00:04 +0100

    Add pseudocode for sprite rendering

--- src/engine/sprites.c
+++ src/engine/sprites.c
@@ -6,7 +6,7 @@
 /*   By: opelser <opelser@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 14:18:28 by opelser           #+#    #+#             */
-/*   Updated: 2024/03/27 17:51:50 by opelser          ###   ########.fr       */
+/*   Updated: 2024/03/27 17:59:08 by opelser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,14 @@ bool		is_sprite_in_view(t_player *player, t_sprite *sprite)
 {
 	(void)player;
 	(void)sprite;
+
 	return (true);
 }
 
 double		get_distance(t_player *player, t_sprite *sprite)
 {
-	double		x;
-	double		y;
-
-	x = sprite->x - player->x;
-	y = sprite->y - player->y;
+	const double	x = sprite->x - player->x;
+	const double	y = sprite->y - player->y;
 
 	return (sqrt(x * x + y * y));
 }
@@ -67,29 +65,54 @@ void		sort_sprites(t_sprite **sprites)
 
 #include <stdio.h>
 
-void	print_sprites(t_sprite *sprites)
+void	print_sprite(t_sprite *sprite)
 {
-	t_sprite	*current;
-
-	current = sprites;
-	while (current)
-	{
-		printf("Sprite at x: %f, y: %f\n", current->x, current->y);
-		current = current->next;
-	}
+	printf("Sprite: %p\n", sprite);
+	printf("X: %f\n", sprite->x);
+	printf("Y: %f\n", sprite->y);
+	printf("Distance: %f\n", sprite->distance);
+	printf("Next: %p\n\n", sprite->next);
 }
 
 void		draw_sprites(t_data *data, t_ray_data *ray_data)
 {
-	// Get all sprites
-	// Calculate their distance to the player
-	// Sort them by distance
-	// Draw the sprites:
-		// 	- Calculate the sprite position on the screen
-		// 	- Calculate the sprite size
-		// 	- Get the line draw data
-		// 	- Draw the sprite line
-
 	(void) ray_data;
-	print_sprites(data->sprites);
+
+	t_sprite	*sprite;
+
+	sprite = data->sprites;
+	while (sprite)
+	{
+		// Calculate  distance to the player
+		sprite->distance = get_distance(&data->player, sprite);
+
+		sprite = sprite->next;
+	}
+
+	// Sort sprites by distance
+	sort_sprites(&data->sprites);
+
+	sprite = data->sprites;
+	while (sprite)
+	{
+		print_sprite(sprite);
+
+		sprite = sprite->next;
+	}
+
+	// Draw the sprites
+	sprite = data->sprites;
+	while (sprite)
+	{
+		// Check if the sprite is in the player view
+		if (is_sprite_in_view(&data->player, sprite))
+		{
+			// Calculate the sprite position on the screen
+			// Calculate the sprite size
+			// Get the line draw data
+			// Draw the sprite line
+		}
+
+		sprite = sprite->next;
+	}
 }
