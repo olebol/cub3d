9fe0c1605e3d0ca9cebd0d9ad9d16b64034d179d
olebol <ole.pelser@gmail.com>
2024-06-04 23:19:13 +0200

    norm (ew)

--- src/engine/draw_sprite.c
+++ src/engine/draw_sprite.c
@@ -6,7 +6,7 @@
 /*   By: opelser <opelser@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 19:51:41 by opelser           #+#    #+#             */
-/*   Updated: 2024/06/04 23:12:36 by opelser          ###   ########.fr       */
+/*   Updated: 2024/06/04 23:19:02 by opelser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,17 +29,13 @@ static void	draw_to_screen(t_data *data, t_ray_data **rays, t_sprite *sprite, \
 		if (sprite->distance > 0 && sprite->distance < rays[x][0].distance)
 		{
 			tex_x = (x - (sprite->screen_x - draw->width / 2)) * draw->step;
-
 			y = draw->start_y;
 			while (y < draw->end_y)
 			{
 				tex_y = (y - (data->mid - draw->height / 2)) * draw->step;
-
 				color = draw->pixels[tex_y * sprite->texture->width + tex_x];
-
 				if ((color & 0x000000FF) != 0)
 					mlx_put_pixel(data->screen, x, y, color);
-
 				y++;
 			}
 		}
@@ -50,20 +46,17 @@ static void	draw_to_screen(t_data *data, t_ray_data **rays, t_sprite *sprite, \
 static void	set_sprite_draw_data(t_data *data, t_sprite *sprite, \
 								t_sprite_draw_data *draw)
 {
-	draw->height = abs((int) (HEIGHT / sprite->distance));
-	draw->width = abs((int) (HEIGHT / sprite->distance));
-
+	draw->height = abs((int)(HEIGHT / sprite->distance));
+	draw->width = abs((int)(HEIGHT / sprite->distance));
 	draw->pixels = (uint32_t *) sprite->texture->pixels + \
 					(sprite->current_frame * sprite->width);
 	draw->step = (double) sprite->width / draw->width;
-
 	draw->start_x = sprite->screen_x - draw->width / 2;
 	if (draw->start_x < 0)
 		draw->start_x = 0;
 	draw->end_x = sprite->screen_x + draw->width / 2;
 	if (draw->end_x >= WIDTH)
 		draw->end_x = WIDTH - 1;
-
 	draw->start_y = data->mid - draw->height / 2;
 	if (draw->start_y < 0)
 		draw->start_y = 0;
@@ -77,6 +70,5 @@ void	draw_sprite(t_data *data, t_ray_data **rays, t_sprite *sprite)
 	t_sprite_draw_data	draw;
 
 	set_sprite_draw_data(data, sprite, &draw);
-
 	draw_to_screen(data, rays, sprite, &draw);
 }
